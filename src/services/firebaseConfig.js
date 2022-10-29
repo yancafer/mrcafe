8615ad539f3857e9430942dbecfaa0ae3899ba85
yancafer <yancafernandes2@gmail.com>
2022-10-29 10:29:56 -0500

    first commit

---
+++ src/services/firebaseConfig.js
@@ -0,0 +1,17 @@
+import { initializeApp } from 'firebase/app';
+import { getAuth } from "firebase/auth";
+
+const firebaseConfig = {
+    apiKey: "AIzaSyBzd7C8EPu71kb9GAk_jszt015yi6BgXkk",
+    authDomain: "mr-cafe-d3bff.firebaseapp.com",
+    projectId: "mr-cafe-d3bff",
+    storageBucket: "mr-cafe-d3bff.appspot.com",
+    messagingSenderId: "520595162806",
+    appId: "1:520595162806:web:5d9aa422fb4b5e95320817"
+};
+
+if (!firebase.apps.length) {
+    firebase.initializeApp(firebaseConfig);
+}
+
+export default firebase;
